f0478fa362a58f05c370a34e734faaf49a02f511
Martin Kinoo <martkinoo@gmail.com>
2024-09-10 13:36:30 +0200

    add realisations/slider, h3 refacto

---
+++ src/components/SlidePagination.tsx
@@ -0,0 +1,13 @@
+type slideEvent = (
+  index: number,
+  firstVisible: number,
+  lastVisible: number,
+) => void;
+
+type Props = {
+  setSlideEvent: (slideEvent: slideEvent) => void;
+};
+
+const SlidePagination = (props: Props) => {
+  return <div class="pagination-wrapper"></div>;
+};
---
+++ src/components/Slider.tsx
@@ -0,0 +1,106 @@
+import {
+  createSignal,
+  For,
+  onCleanup,
+  onMount,
+  type Accessor,
+} from "solid-js";
+import BlazeSlider from "blaze-slider";
+import "blaze-slider/dist/blaze.css";
+import "./Slider.css";
+
+type PaginationProps = {
+  pictures: string[];
+  index: Accessor<number>;
+  moveTo: (index: number) => void;
+};
+
+const SliderPagination = (props: PaginationProps) => {
+  return (
+    <div class="pagination-wrapper">
+      <div class="pagination" aria-hidden="true">
+        <ul class="list">
+          <For each={props.pictures}>
+            {(_, index) => (
+              <li
+                classList={{ active: index() === props.index() }}
+                onClick={() => props.moveTo(index())}
+              ></li>
+            )}
+          </For>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+type SliderProps = {
+  pictures: string[];
+  children: HTMLElement;
+};
+
+const Slider = (props: SliderProps) => {
+  let sliderEl: HTMLDivElement;
+  let unsubscribe: () => void;
+  const [currentIndex, setCurrentIndex] = createSignal(0);
+  const [slider, setSlider] = createSignal<BlazeSlider>();
+
+  const moveTo = (idx: number) => {
+    const s = slider();
+    if (!s) return;
+    setCurrentIndex(idx);
+    const currentIndex = s.stateIndex;
+    const diff = Math.abs(currentIndex - idx);
+    if (currentIndex < idx) {
+      s.next(diff);
+    } else {
+      s.prev(diff);
+    }
+  };
+
+  onMount(() => {
+    const slider = new BlazeSlider(sliderEl);
+
+    setSlider(slider);
+    unsubscribe = slider.onSlide((pageIndex, fIndex, lIndex) => {
+      console.log(pageIndex, fIndex, lIndex)
+      if (currentIndex() !== pageIndex) setCurrentIndex(pageIndex);
+    });
+  });
+
+  onCleanup(() => {
+    if (unsubscribe) {
+      unsubscribe();
+    }
+  });
+
+  return (
+    <div ref={sliderEl!}>
+      <div class="blaze-container">
+        <div class="blaze-track-container">
+          <div class="blaze-track">
+            <For each={props.pictures}>
+              {(picture) => (
+                <div>
+                  <img src={`images/${picture}`} alt={picture} />
+                </div>
+              )}
+            </For>
+          </div>
+        </div>
+
+        <div class="blaze-navigation">
+          <button class="blaze-prev">{props.children}</button>
+          <SliderPagination
+            pictures={props.pictures}
+            index={currentIndex}
+            moveTo={moveTo}
+          />
+          <button class="blaze-next">{props.children}</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;
